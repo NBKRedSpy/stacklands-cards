61ee78c779ba4f6bd23eae044dbe2e73119af41d
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-09-23 16:26:52 -0400

    Added resource match indicator
    Added a separator between matches on a Card's name and a resource used
    by a card.
    
    Extracted various logic to controls.
    Removed filter label.  Input placeholder is sufficient.
    Moved card filtering from CardDataService to UI since it is a UI concern.

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,13 @@
 import './App.css';
-import Card from './components/Card'
-import { CardDataService } from './services/CardDataService';
-import CardFilter from './components/CardFilter'
-import * as c from './classes/Card'
-import React from 'react'
-
-const cardDataService = new CardDataService();
-
-function App() {
-    const [cardData, setCardData] = React.useState<c.Card[]>();
-
-    React.useEffect(() => {
-      async function cb() {
-        // const cardData = await cardDataService.getAll()
-        setCardData(await cardDataService.getAll());
-      }
-      cb();
-
-      
-
-    }, []);
+import React from 'react';
+import CardList from './Pages/CardList/CardList'
 
 
+export default function App() {
     return (
-      <div>
-        <div><CardFilter onChange={filterChange} /></div>
-        <div>
-          {/* {cardData && cardData.map(x=> <div key={x.key}><Card card={x}/></div>)} */}
-          {cardData && cardData.map(x=> <Card key={x.key} card={x}/>)}
-        </div>
-
-      </div>
+      <CardList></CardList>
     );
-
-    async function filterChange(event: React.ChangeEvent<HTMLInputElement>)
-    {
-      if(event.target.value)
-      {
-        const cardData: c.Card[] = await cardDataService.getFiltered(event.target.value);
-
-        setCardData(cardData);
-      }
-      else  
-      {
-        setCardData(await cardDataService.getAll());
-      }
-      
-    }    
 }
 
 
 
-export default App;
