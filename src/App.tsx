b5113f0ba39357bbe9f1c8d9101f4c28d59a93a3
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-11-02 14:35:44 -0400

    Changed counter to Google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
 import React from 'react';
 import CardList from './Pages/CardList/CardList'
-import Counter from './components/Counter'
 
 
 export default function App() {
     return (
       <>
         <CardList></CardList>
-        <Counter />
       </>
 
     );
